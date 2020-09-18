d45a64051165c2029818e217b975e65062439f61
prasad katti <praskatti@yahoo.com>
2020-09-18 23:35:40 +0530

    changed on feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hi there ! Finally on elastic beanstalk
+        Hi there ! i was changed on feature branch
         </p>
         <a
           className="App-link"
